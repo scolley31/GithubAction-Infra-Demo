3a93808d59a6e3cb8bb8b237cf76a99fd607a3ef
scolley <scolley.huang@kkday.com>
2024-04-18 14:30:04 +0800

    test

--- variables.tf
+++ variables.tf
@@ -2,3 +2,18 @@ variable "region" {
   description = "AWS region"
   type        = string
 }
+
+variable "vpc_cidr" {
+  description = "VPC CIDR for the VPC"
+  type        = string
+}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for the VPC"
+  type        = list(string)
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for the VPC"
+  type        = list(string)
+}
