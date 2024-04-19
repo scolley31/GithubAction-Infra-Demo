fea7eee63a5dd45d156b3e4560261f3bc04d96ba
scolley <scolley.huang@kkday.com>
2024-04-20 02:03:46 +0800

    add remote backend

---
+++ s3/s3.tf
@@ -0,0 +1,22 @@
+resource "aws_s3_bucket" "state" {
+  bucket_prefix = "terraform-state"
+
+  tags = {
+    Terraform = true
+  }
+}
+
+resource "aws_dynamodb_table" "state_locks" {
+  name         = "terraform-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Terraform = true
+  }
+}
